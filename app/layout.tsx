6aaef0071634570289ce8c4b9b029a32bd3f5bf1
JasonSnow123 <m.jason.2018@gmail.com>
2024-04-08 14:57:33 -0400

    Adds user layout login to site

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ToasterContext from "./context/toast-context";
+import AuthContext from "./context/auth-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <>
+        <AuthContext>
           <ToasterContext />
           {children}
-        </>
+        </AuthContext>
       </body>
     </html>
   );
