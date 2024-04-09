eee2fed485daa5e8756dea6e546feea3ce34dce5
JasonSnow123 <m.jason.2018@gmail.com>
2024-04-09 17:03:30 -0400

    Adds mobile and desktop sidebar

--- components/sidebar/side-bar.tsx
+++ components/sidebar/side-bar.tsx
@@ -1,3 +1,12 @@
+import { DesktopSideBar } from "./desktop-sidebar";
+import { MobileSideBar } from "./mobile-sidebar";
+
 export async function SideBar({ children }: { children: React.ReactNode }) {
-  return <div className="h-full">{children}</div>;
+  return (
+    <div className="h-full">
+      <MobileSideBar />
+      <DesktopSideBar />
+      <main className="lg:pl-20 h-full">{children}</main>
+    </div>
+  );
 }
