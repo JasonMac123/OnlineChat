bdfcb5b8810237bcb7b217b5ace9df8cdd4b1bf0
JasonSnow123 <m.jason.2018@gmail.com>
2024-04-11 19:45:36 -0400

    Creates user avatar that will display user avatar image

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -1,3 +1,19 @@
-export const UserAvatar = () => {
-  return <div></div>;
+"use client";
+
+import { User } from "@prisma/client";
+import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
+
+interface UserAvatarProps {
+  user: User | null;
+}
+
+export const UserAvatar = ({ user }: UserAvatarProps) => {
+  return (
+    <div className="relative">
+      <Avatar>
+        <AvatarImage src={user?.image || "/images/placeholder.png"} />
+        <AvatarFallback />
+      </Avatar>
+    </div>
+  );
 };
