94b86f50a0db70288277baa5d9f3e00800515f98
JasonSnow123 <m.jason.2018@gmail.com>
2024-04-12 14:54:36 -0400

    Creates an user list to display all users with ongoing chats

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,8 +1,14 @@
+import getUsers from "../actions/getUsers";
+
 import { SideBar } from "@/components/sidebar/side-bar";
+import { UserList } from "./components/user-list";
 
 const UsersLayout = async ({ children }: { children: React.ReactNode }) => {
+  const users = await getUsers();
+
   return (
     <SideBar>
+      <UserList users={users}/>
       <div className="h-full">{children}</div>;
     </SideBar>
   );
