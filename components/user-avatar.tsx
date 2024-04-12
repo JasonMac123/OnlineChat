b40912917f3ccaea9497333b0fefb5db7dcd6a9c
JasonSnow123 <m.jason.2018@gmail.com>
2024-04-11 20:34:08 -0400

    Displays green icon for active user showing user status

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -14,6 +14,7 @@ export const UserAvatar = ({ user }: UserAvatarProps) => {
         <AvatarImage src={user?.image || "/images/placeholder.png"} />
         <AvatarFallback />
       </Avatar>
+      <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3" />
     </div>
   );
 };
