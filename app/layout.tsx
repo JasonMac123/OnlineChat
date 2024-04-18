58fae7a44be6c91f42b2bea69a0259e7ae2efc77
JasonSnow123 <m.jason.2018@gmail.com>
2024-04-18 15:20:56 -0400

    Styles toast component based on message type

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthContext>
-          <Toaster />
+          <Toaster position="top-center" richColors />
           {children}
         </AuthContext>
       </body>
